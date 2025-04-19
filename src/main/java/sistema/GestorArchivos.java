016a7b80b18889a5988b522f96d41ed0d30c3e8f
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-04-19 15:42:00 -0400

    fix: corregir error al escribir y leer objetos
    se modificaron los metodos para que sean más simples

--- src/main/java/sistema/GestorArchivos.java
+++ src/main/java/sistema/GestorArchivos.java
@@ -1,10 +1,11 @@
 package sistema;
 
-import com.fasterxml.jackson.databind.JsonNode;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -17,75 +18,31 @@ public class GestorArchivos {
     private GestorArchivos() {
     }
 
+    //Metodo para nombrar archivos dependiendo el tipo de objeto
+    private static <T> String nombrarArchivo(Class<T> tipo) {
+        return tipo.getSimpleName() + ".json";
+    }
 
-    //Revisa si el archivo existe, si existe lo sobreescribe, si no existe lo crea
-    public static <T> void guardarArchivo(T objeto) {
-        String nombreArchivo = objeto.getClass().getSimpleName();
-        if (archivoExiste(nombreArchivo)) {
-            sobreEscribirArchivo(objeto, nombreArchivo);
-            return;
-        }
-
-        List<T> nuevaLista = new ArrayList<>();
-        nuevaLista.add(objeto);
-        try {
-            mapper.writeValue(new File(nombreArchivo), nuevaLista);
-        } catch (IOException e) {
-            logger.warning("Error al guardar el objeto en el archivo " + nombreArchivo + ": " + e.getMessage());
+    //Metodo para escribir una lista de objetos en un archivo
+    public static <T> void escribirListaObjetos(List<T> listaObjetos, Class<T> tipo) {
+        String nombreArchivo = nombrarArchivo(tipo);
+        try{
+            mapper.writeValue(new File(nombreArchivo), listaObjetos);
+            logger.info("Archivo guardado de forma satisfactoria");
+        }catch (Exception e){
+            logger.warning("Error al escribir el archivo: "+ nombreArchivo +": "+ e.getMessage());
         }
     }
 
-    //sobre escribre el archivo existente
-    public static <T> void sobreEscribirArchivo(T objeto, String nombreArchivo) {
-        List<T> datosArchivo = leerListaObjetoArchivo(nombreArchivo, (Class<T>) objeto.getClass());
-
-        if (datosArchivo.isEmpty()) {
-            datosArchivo = new ArrayList<>();
-        }
-
-        datosArchivo.removeIf(obj -> obj.equals(objeto));
-        datosArchivo.add(objeto);
-
-        try {
-            mapper.writeValue(new File(nombreArchivo), datosArchivo);
-        } catch (IOException e) {
-            logger.warning("Error al sobrescribir el archivo " + nombreArchivo + ": " + e.getMessage());
-        }
-
-    }
-
-
-    //lee el archivo y lo convierte a una lista de objetos
-    public static <T> List<T> leerListaObjetoArchivo(String nombreArchivo, Class<T> claseTipo) {
-        List<T> listaObjetos = new ArrayList<>();
-        try {
-            JsonNode root = mapper.readTree(new File(nombreArchivo));
-            if (root.isArray()) {
-                listaObjetos = mapper.convertValue(root, mapper.getTypeFactory().constructCollectionType(List.class, claseTipo));
-            } else if (root.isObject()) {
-                T objeto = mapper.convertValue(root, claseTipo);
-                listaObjetos = new ArrayList<>();
-                listaObjetos.add(objeto);
-            }
-        } catch (IOException e) {
-            logger.warning("Error al leer el archivo " + nombreArchivo + ": " + e.getMessage());
-        }
-        return listaObjetos;
-    }
-
-    //lee el archivo y lo convierte a un objeto
-    public static <T> T leerArchivo(String nombreArchivo, Class<T> claseTipo) {
-        try {
-            return mapper.readValue(new File(nombreArchivo), claseTipo);
-        } catch (Exception e) {
-            logger.warning("Error al leer el personaje desde " + nombreArchivo + ": " + e.getMessage());
-            return null;
+    //Metodo para leer una lista de objetos de un archivo
+    public static <T> List<T> leerListaObjetos(String nombreArchivo, Class<T> tipo) {
+        try{
+            return mapper.readValue(new File(nombreArchivo),
+                    mapper.getTypeFactory().constructCollectionType(List.class, tipo));
+        }catch(Exception e){
+            logger.warning("Error al leer el archivo");
+            return Collections.emptyList();
         }
     }
 
-    //verifica si el archivo existe
-    public static boolean archivoExiste(String nombreArchivo) {
-        File nuevoArchivo = new File(nombreArchivo);
-        return nuevoArchivo.exists();
-    }
 }
