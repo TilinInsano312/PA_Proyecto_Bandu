805238e92ae44b2d83b8ff145502db8cb736eacb
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-04-21 23:53:16 -0400

    feat: agregar modificarUnAtributoPerfil

--- src/main/java/sistema/GestorArchivos.java
+++ src/main/java/sistema/GestorArchivos.java
@@ -2,9 +2,11 @@ package sistema;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import modelos.usuario.PerfilUsuario;
 
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
@@ -45,6 +47,7 @@ public class GestorArchivos {
         }
     }
 
+    // Metodo para borrar un objeto de un archivo
     public static <T> void borrarObjeto(String nombreArchivo, Class<T> tipo, T objeto) {
         List<T> listaObjetos = leerListaObjetos(nombreArchivo, tipo);
         try{
@@ -57,5 +60,30 @@ public class GestorArchivos {
 
     }
 
+    //Metodo para modificar un PerfilUsuario
+    public static PerfilUsuario modificarUnAtributoPerfilUsuario(PerfilUsuario perfilUsuario,String nombreAtributo, Object nuevoValor) {
+        String archivo  = "PerfilUsuario.json";
+        borrarObjeto(archivo, PerfilUsuario.class, perfilUsuario);
+        List<PerfilUsuario> listaPerfiles = leerListaObjetos(archivo, PerfilUsuario.class);
+
+        PerfilUsuario perfilModificar = perfilUsuario;
+        try{
+            Field atributo = PerfilUsuario.class.getDeclaredField(nombreAtributo);
+            atributo.setAccessible(true);
+            atributo.set(perfilModificar, nuevoValor);
+
+            listaPerfiles.remove(perfilUsuario);
+            listaPerfiles.add(perfilModificar);
+
+            escribirListaObjetos(listaPerfiles, PerfilUsuario.class);
+            logger.info("Atributo modificado de forma satisfactoria");
+            return perfilUsuario;
+        }catch (Exception e){
+            logger.warning("Error al modificar el atributo: "+ nombreAtributo + ": " + e.getMessage());
+        }
+
+        return null;
+
+    }
 
 }
