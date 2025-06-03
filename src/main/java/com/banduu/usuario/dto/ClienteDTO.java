3b987b3ace5eb3f3ff154dbe1bc212118b3e0b2c
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-06-03 11:50:54 -0400

    feat: DTO's Usuario

---
+++ src/main/java/com/banduu/usuario/dto/ClienteDTO.java
@@ -0,0 +1,25 @@
+package com.banduu.usuario.dto;
+
+import com.banduu.musica.modelos.Album;
+import com.banduu.musica.modelos.Artista;
+import com.banduu.musica.modelos.Cancion;
+
+import java.util.List;
+
+public record ClienteDTO(
+
+        String id,
+         String nombre,
+         String apellido,
+         int edad,
+         String carrera,
+         boolean mismaCarrera,
+         String orientacion,
+         String genero,
+         List<String>generosMusicales,
+         List<Cancion> canciones,
+         List<Artista> artistas,
+         List<Album> albums
+
+) {
+}
