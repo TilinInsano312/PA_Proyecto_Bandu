c876074d18ed96972364d3a0e409bfa9c22d8717
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-06-14 19:57:47 -0400

    feat: agregacion de metodo findByNombreUsuario

--- src/main/java/com/banduu/usuario/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/banduu/usuario/repositorios/UsuarioRepositorio.java
@@ -5,13 +5,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @EnableMongoRepositories
 @Repository
 public interface UsuarioRepositorio extends MongoRepository<Usuario, String> {
-
-    Usuario insert(Usuario usuario);
-    List<Usuario> findAll();
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
 
 }
