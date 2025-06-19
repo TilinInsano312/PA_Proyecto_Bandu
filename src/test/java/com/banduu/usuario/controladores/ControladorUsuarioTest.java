b0271762912c8fc67caa49414059b77ddfa64628
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-06-19 18:04:17 -0400

    refactor: Clase ControladorUsuarioTest y SecurityConfig

--- src/test/java/com/banduu/usuario/controladores/ControladorUsuarioTest.java
+++ src/test/java/com/banduu/usuario/controladores/ControladorUsuarioTest.java
@@ -1,28 +1,37 @@
 package com.banduu.usuario.controladores;
 
+import com.banduu.usuario.servicios.ServicioUsuario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ControladorUsuarioTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ServicioUsuario servicioUsuario;
 
+    @BeforeEach
+    public void setUp() {
+        servicioUsuario.findAll().forEach(usuario -> servicioUsuario.delete(usuario.id()));
+    }
     @Test
     public void testListarUsuarios() throws Exception {
+        servicioUsuario.save( new com.banduu.usuario.dto.UsuarioDTO("1", "user1", "pass1", "a"));
         mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/api/usuario").header("Authorization", "no Auth" ))
-                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().isOk())
-                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.content().contentType("application/json"));
+                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().isOk());
     }
     @Test
     public void testBuscarUsuarioPorId() throws Exception {
+        servicioUsuario.save( new com.banduu.usuario.dto.UsuarioDTO("1", "user1", "pass1", "a"));
         mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/api/usuario/1").header("Authorization", "no Auth" ))
-                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().isOk())
-                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.content().contentType("application/json"));
+                .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().isOk());
     }
     @Test
     public void testGuardarUsuario() throws Exception {
