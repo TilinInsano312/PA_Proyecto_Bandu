68803ab40d4e09a67bb6f5f59db298d05c536387
Vicente Salazar <v.salazar11@ufromail.cl>
2025-07-04 18:02:00 -0400

    refactor: metodos update usuario

--- src/main/java/com/banduu/usuario/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/banduu/usuario/repositorios/UsuarioRepositorio.java
@@ -1,15 +1,24 @@
 package com.banduu.usuario.repositorios;
 
+import com.banduu.musica.modelos.Album;
 import com.banduu.usuario.modelos.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @EnableMongoRepositories
 @Repository
 public interface UsuarioRepositorio extends MongoRepository<Usuario, String> {
     Optional<Usuario> findByNombreUsuario(String nombreUsuario);
-
+    @Query("{'id': ?0}")
+    @Update("{'$set': {'contrasena': ?1}}")
+    void updateContrasenaById(String contrasena, int id);
+    @Query("{'id': ?0}")
+    @Update("{'$set': {'email': ?1}}")
+    void updateEmailById(String email, int id);
 }
