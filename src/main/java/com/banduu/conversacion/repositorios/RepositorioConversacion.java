2b086ef0f580d4f3f0d50e0d4abb29d8495a5cd7
SebaS01010101 <s.sandoval18@ufromail.cl>
2025-07-05 19:00:07 -0400

    feat: agregar consulta para encontrar conversación según participantes
    .

--- src/main/java/com/banduu/conversacion/repositorios/RepositorioConversacion.java
+++ src/main/java/com/banduu/conversacion/repositorios/RepositorioConversacion.java
@@ -4,12 +4,14 @@ import com.banduu.conversacion.modelos.Conversacion;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
 public interface RepositorioConversacion extends MongoRepository<Conversacion, String> {
 
-    Optional<Conversacion> findByIdRemitenteAndIdDestinatario(String idRemitente, String idDestinatario);
+    @Query("{ $or: [ " +
+            "{ 'idRemitente': ?0, 'idDestinatario': ?1 }, " +
+            "{ 'idRemitente': ?1, 'idDestinatario': ?0 } " +
+            "] }")
+    Optional<Conversacion> findByParticipantes(String usuario1, String usuario2);
 }
